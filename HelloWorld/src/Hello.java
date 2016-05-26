e93c04470b8a227cc63f2d79eaea6ca72c739332
Kim Ulrich <kim.ulrich@skat.dk>
2016-05-26 20:01:30 +0200

    Test commit 5

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		// dette er en ændring til v.01 branch
+		// dette er en ny ændring til v.01 branch
 	}
 
 }
