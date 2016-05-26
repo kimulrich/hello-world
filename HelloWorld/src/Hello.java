e9ce6a67863967b62fdce3825e7646e8b6becd02
kimulrich <kim.ulrich@skat.dk>
2016-05-26 20:08:08 +0200

    Update Hello.java

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,9 +3,10 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		// dette er en ændring til v.01 branch
-		// dette er en ny ændring til v.01 branch
-		// dette er en ændring til v.02 branch
+		// dette er en Ã¦ndring til v.01 branch
+		// dette er en ny Ã¦ndring til v.01 branch
+		// dette er en Ã¦ndring til v.02 branch
+		// dette er v.03 branch
 	}
 
 }
